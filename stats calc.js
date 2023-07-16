9407a3e04cfdf2c13d8d9a29bddbcdb48822ae99
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2023-07-16 13:55:37 -0500

    Add files via upload

---
+++ stats calc.js
@@ -0,0 +1,27 @@
+/*  take as integer input the number of times a occurs. provide field.
+    take as integer input the number of times b occurs. provide field.
+    take as integer input the number of times c occurs. provide field.
+    take as integer input the total number of intervals
+
+    -what to do if one of them is left blank?
+        -if total intervals is blank, return error
+    -what to do if one or more has zero as an input?
+        -f x is zero, then x intersect anything else is zero
+
+
+    calculate and provide a intersect b
+    calculate and provide b intersect c
+    calculate and provide a intersect c
+    calculate and provide a intersect b intersect c
+    calculate and provideP(a given b)
+    calculate and provide P(a given c)
+    calculate and provide P(b given c)
+    calculate and provide P(c given a)
+    calculate and provide P(c given b)
+    calculate and provide P(b given a)
+    calculate and provide equation 2.
+
+    -a button to clear all fields.
+    -
+
+    */
