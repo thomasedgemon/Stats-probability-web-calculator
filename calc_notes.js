2b271f8b979acb86e94928bc5527c0bbe037bd40
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2023-07-19 15:51:15 -0500

    Add files via upload

---
+++ calc_notes.js
@@ -0,0 +1,12 @@
+//instantiate buttons, fields, gui framework, copyright notes, etc
+
+// function clear(onbutton "clear" press);
+// clear all fields and their associated vars
+
+// function calculate(onbutton "calculate" press);
+// perform calculation
+
+// if "calculate" button pressed
+// calculate()
+// elif "clear" button pressed
+// clear()
