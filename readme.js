27d6b11d03dc029915015a6b70bd77536e4fe39f
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2023-08-13 11:44:01 -0500

    Update readme.js

--- readme.js
+++ readme.js
@@ -1,8 +1,9 @@
 /*
 
-This is a calculator for conditional probabilities made 
-for use in behavior sciences and related fields.
+This is a calculator for conditional probabilities as defined 
+specifically within the field of Behavior Analysis. These calculations cannot 
+be used for generic, standard conditional probability purposes.
 
-It is written in JavaScript and React
+It is written in JavaScript and CSS with React.js
 
 */
